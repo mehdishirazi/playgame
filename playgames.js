6a8a3561314897581863d83a7501d02ac503988b
mehdi shirazi <msh199271@gmail.com>
2019-10-31 13:54:49 +0330

    Added new version

--- playgames.js
+++ playgames.js
@@ -1,9 +1,20 @@
-let second = document.getElementById('numberone').textContent
-let countdown = setInterval(Count(), 1000)
-function Count() {
+let second = document.getElementById('numberone').textContent;
+let countdown = setInterval(count ,1000);
+let clickone = document.getElementById('numberone');
+
+
+function count() {
     second--;
     document.getElementById('numberone').textContent = second;
     if (second <= 0) {
         clearInterval(countdown);
     }
 }
+
+
+function restNumber(){
+    document.getElementById('numberone').textContent = 10;
+} 
+
+
+clickone.onclick = restNumber;
