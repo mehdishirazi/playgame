0408009c5f1463e5ee9fffe2b55d10d79b4823dd
mehdi shirazi <msh199271@gmail.com>
2019-11-01 13:26:53 +0330

    Fixed some syntax Error

--- playgames.js
+++ playgames.js
@@ -1,20 +1,26 @@
-let second = document.getElementById('numberone').textContent;
-let countdown = setInterval(count ,1000);
-let clickone = document.getElementById('numberone');
+let one = document.getElementById('numberOne').textContent
+let two = document.getElementById('numberTwo').textContent
+let three = document.getElementById('numberThree').textContent
+let four = document.getElementById('numberFour').textContent
+let five = document.getElementById('numberFive').textContent
+let countDown = setInterval(count ,1000);
+let clickone = document.getElementById('numberOne'); 
 
 
 function count() {
-    second--;
-    document.getElementById('numberone').textContent = second;
-    if (second <= 0) {
-        clearInterval(countdown);
+    one--; 
+    two--; 
+    document.getElementById('numberOne').textContent = one; 
+    document.getElementById('numberTwo').textContent = two; 
+    if (one <= 0 || two <= 0) {
+        clearInterval(countDown);
     }
 }
 
 
 function restNumber(){
-    document.getElementById('numberone').textContent = 10;
-    second = document.getElementById('numberone').textContent;
+    document.getElementById('numberOne').textContent = 10;
+    one = document.getElementById('numberOne').textContent;
 } 
 
 
