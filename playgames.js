14cab28adca0c0c294436dea79175990a5bf15e4
Mehdi Shirazi <msh199271@gmail.com>
2019-11-19 16:03:13 +0330

    Added new value

--- playgames.js
+++ playgames.js
@@ -1,49 +1,26 @@
 let countDown = setInterval(Count ,1000);
-let btnone = document.getElementById('btnOne'); 
-let btntwo = document.getElementById('btnTwo');
-let btnthree = document.getElementById('btnThree');
-let btnfour = document.getElementById('btnFour');
-let btnfive = document.getElementById('btnFive');
-let btnsix = document.getElementById('btnSix');
-let btnseven = document.getElementById('btnSeven');
-let btneight = document.getElementById('btnEight');
-let btnnine = document.getElementById('btnNine');
-
+for(i=1; i < 10; i++){
+    id = 'btnNumber' + i;
+  document.getElementById(id).onclick = RestNumber;
+}
 
 function Count() {
-
-    for(let i = 0; i <= 10; i++){
-        console.log(document.getElementById('btnOne'))
-        --document.getElementById('btnOne').value; 
-    }
-    if(
-        btnone <= 0 || 
-        btntwo <= 0 || 
-        btnthree <= 0 || 
-        btnfour <= 0 || 
-        btnfive <= 0 || 
-        btnsix <=0 || 
-        btnseven <= 0 || 
-        btneight <= 0
-    ){
-        clearInterval(countDown);
-    }
+    for(i=1; i < 10; i++){
+        id = 'btnNumber' + i;
+        document.getElementById(id).value = document.getElementById(id).value - 1;
+        if(document.getElementById(id).value <= 0){
+              if(document.getElementById(id).value === '0'){
+                document.getElementById(id).style.background = 'red';
+            }
+            clearInterval(countDown);
+        }
+    } 
 }
 
 
 function RestNumber(e){
   debugger
-    document.getElementById(e.currentTarget.id).textContent = 10;
+    document.getElementById(e.currentTarget.id).value= 10;
 } 
 
-
-btnone.onclick = RestNumber;
-btntwo.onclick = RestNumber;
-btnthree.onclick = RestNumber;
-btnfour.onclick = RestNumber;
-btnfive.onclick = RestNumber;
-btnsix.onclick = RestNumber;
-btnseven.onclick = RestNumber;
-btneight.onclick = RestNumber;
-btnnine.onclick = RestNumber;
 
