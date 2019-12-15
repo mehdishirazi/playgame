db1921b4ddcbd513bb48e4dfeb54c49b45a8ff31
mehdi shirazi <msh199271@gmail.com>
2019-12-15 23:27:16 +0330

    Added New changes

--- playgames.js
+++ playgames.js
@@ -1,11 +1,11 @@
 let run = document.getElementById('btnRun');
-run.onclick = Run;
-let tableNumber = null;
+run.onclick = Start;
 let timer;
 let countDown = null; 
 
 
 function CreateBtn(tableNumber){
+  debugger
     for(i = 1; i <= tableNumber**2; i++){
         let btn = document.createElement("BUTTON");
         btn.id = "btnNumber" + i;
@@ -17,28 +17,27 @@ function CreateBtn(tableNumber){
 }
 
 
-function ClearBtn(){
+function ClearBtn(tableNumber){
+  debugger
     for(i = 1; i <= tableNumber**2; i++){
         id = 'btnNumber' + i;
         BTN = document.getElementById(id);
         BTN.parentNode.removeChild(BTN);
     }
-          tableNumber = null;
-          CreateBtn();
 }
 
 
-function Run(){
+function Start(){
+    timer = document.getElementById('Timer').innerHTML; 
     countDown = setInterval(Counter, 1000);
     let timeLeft = setInterval(Time, 1000);
-    Time();
-    init();
+    Time(timer);
     Counter();
 }
 
 
-function Time(){
-    timer = document.getElementById('Timer').innerHTML; 
+function Time(timer){
+  debugger
     timer = timer -1;
     document.getElementById('Timer').innerHTML = timer;
     if(timer < 0){
@@ -50,7 +49,6 @@ function Time(){
     
     
 function Init(){
-  debugger
     tableNumber = document.getElementById('dropdown').value;
     CreateBtn(tableNumber);
 }
