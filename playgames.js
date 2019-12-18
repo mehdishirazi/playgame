014f317d66e2bb189d84eefd02405f59436f25df
mehdishirazi <49481094+mehdishirazi@users.noreply.github.com>
2019-12-18 12:30:20 +0330

    Feature (#3)
    * Fixing bug on creating buttons
    
    * Fixed time bug

--- playgames.js
+++ playgames.js
@@ -1,63 +1,44 @@
-let run = document.getElementById('btnRun');
-run.onclick = Start;
 let timer;
 let countDown = null; 
+let timeLeft = null;
+let tableNumber = null;
 
 
 function CreateBtn(tableNumber){
     for(i = 1; i <= tableNumber**2; i++){
-        let btn = document.createElement("BUTTON");
+        let btn = document.createElement('BUTTON');
         btn.id = "btnNumber" + i;
         btn.value = 10;
         btn.innerHTML = 10;
+        btn.onclick = RestNumber;
         document.getElementById('buttons').appendChild(btn); 
-        document.getElementById(btn.id).onclick = RestNumber;
-    }  
-}
-
-
-function ClearBtn(){
-    for(i = 1; i <= tableNumber**2; i++){
-        id = 'btnNumber' + i;
-        BTN = document.getElementById(id);
-        BTN.parentNode.removeChild(BTN);
     }
-          tableNumber = null;
-          CreateBtn();
-}
-
-
-function Run(){
-    countDown = setInterval(Counter, 1000);
-    let timeLeft = setInterval(Time, 1000);
-    Time();
-    init();
 }
 
 
 function ClearBtn(tableNumber){
-  debugger
     for(i = 1; i <= tableNumber**2; i++){
         id = 'btnNumber' + i;
         BTN = document.getElementById(id);
         BTN.parentNode.removeChild(BTN);
     }
+    document.getElementById('dropdown').disabled = false;
+    document.getElementById('Timer').innerHTML = 45;
+    clearInterval(timeLeft);
+    clearInterval(countDown);
 }
 
 
 function Start(){
-    timer = document.getElementById('Timer').innerHTML; 
     countDown = setInterval(Counter, 1000);
-    let timeLeft = setInterval(Time, 1000);
-    Time(timer);
-    Counter();
+    timeLeft = setInterval(Time, 1000);
 }
 
 
-function Time(timer){
-    timer = timer -1;
-    document.getElementById('Timer').innerHTML = timer;
-    if(timer < 0){
+function Time(){
+    timer = document.getElementById('Timer'); 
+    timer.innerHTML = timer.innerHTML -1;
+    if(timer.innerHTML <= 0){
         clearInterval(timeLeft);
         alert ("You lOst");
         return;
@@ -67,6 +48,9 @@ function Time(timer){
     
 function Init(){
     tableNumber = document.getElementById('dropdown').value;
+    if(tableNumber !== null){
+        document.getElementById('dropdown').disabled = true;
+    }
     CreateBtn(tableNumber);
 }
     
